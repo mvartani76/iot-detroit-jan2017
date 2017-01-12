5079937477fb4660361c0b371a21ccb06b429c4f
mvartani76 <mike.vartanian@gmail.com>
2017-01-12 19:06:54 +0000

    Changed image references to file references to make generic for this example code.

--- AWS_s3/aws_s3_file_upload.py
+++ AWS_s3/aws_s3_file_upload.py
@@ -9,24 +9,24 @@ aws_secret_key = "your-aws-secret-key"
 REGION_HOST = "your-region-host"
 
 
-def upload_image_to_s3(image_file_path, bucket_name):
+def upload_file_to_s3(file_file_path, bucket_name):
 	"""Uploads images to Amazon's S3 service.
  
 	Arguments:
-	image_file_path: Path to image to upload on local machine.
-	bucket_name: Name of the S3 bucket where image should be uploaded.
+	file_file_path: Path to file to upload on local machine.
+	bucket_name: Name of the S3 bucket where file should be uploaded.
 	key_name: Name of the key for the file on S3 (usually the
 			timestamp).
 	"""
 	bucket = s3_connection.get_bucket(bucket_name)
  
-	# Create a new key using image_file_path as the key
+	# Create a new key using file_file_path as the key
 	key = Key(bucket)
-	key.key = image_file_path 
-	key.set_contents_from_filename(image_file_path)
+	key.key = file_file_path 
+	key.set_contents_from_filename(file_file_path)
 	return key
 
 
 s3_connection = S3Connection(aws_access_key_id, aws_secret_key, host=REGION_HOST)
 
-s3_key = upload_image_to_s3("your-file-to-upload", s3_bucket)
+s3_key = upload_file_to_s3("your-file-to-upload", s3_bucket)
